6191e6474105168bfc9e1e545b2addfd78eb9a2f
kiiiu15 <tinbulzomi@hotmail.com>
2022-02-05 21:18:49 -0300

    update gitignore

---
+++ api/src/Domain/Author.ts
@@ -0,0 +1,27 @@
+class Author {
+
+    constructor(
+        private id: number,
+        private name: string
+    ) { }
+
+
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+
+}
---
+++ api/src/Domain/Book.ts
@@ -0,0 +1,41 @@
+class Book {
+    constructor(
+        private id: number,
+        private name: string,
+        private description: string,
+        private author: Author
+    ) { }
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public setDescription(description: string): void {
+        this.description = description;
+    }
+
+    public getAuthor(): Author {
+        return this.author;
+    }
+
+    public setAuthor(author: Author): void {
+        this.author = author;
+    }
+
+}
